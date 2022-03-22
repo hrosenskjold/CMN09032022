daa977348cfac472991cc888234ab4f86bec1670
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2022-03-22 10:53:54 +0100

    Add files via upload

---
+++ data/Aalegraes_1.js
@@ -0,0 +1,24 @@
+var json_Aalegraes_1 = {
+    "type":"FeatureCollection","name":"Aalegraes_1",
+    "crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},
+
+    "features":[{"type":"Feature",
+    "properties":{"id":"1",
+    "Lokalitet":"Odense Fjord",
+    "Aktører":"SDU, AU",
+    "Finansieri":"Landbrugspakke",
+    "Status":"Gennemført","Areal":"?",
+    "Beskriv":null},
+    "geometry":{"type":"Point",
+    "coordinates":[10.526687583770636,55.489716395760887]}},
+    
+    {"type":"Feature",
+    "properties":{"id":"3",
+    "Lokalitet":"Horsens Fjord",
+    "Aktører":"SDU, AU",
+    "Finansieri":"Landbrugspakke",
+    "Status":"Gennemført",
+    "Areal":"2 ha",
+    "Beskriv":"Horsens Fjord har blandt mange andre fjorde dannet grundlag for studier af de mange presfaktorer og deres tærskelværdier i forhold til naturgenopretning af ålegræs. Her blev det første marine storskala naturgenopretningsområde anlagt med ca. 17.000 skud, som efterfølgende er moniteret, så vi nu har kvantificeret alle relevante økosystemtjenester i forhold til den nøgne bund (baseline): kulstofbegravelse, vækstbaseret C, N, P-optag i ålegræs, samt stigende faunatætheder, fødekædeudviklingen og den forbedrede biodiversitet. <br><br> Som en del af ålegræsudplantningen er der gennemført gradient-studier samtidig med at der er lavet test-transplantationer af ålegræs. <br><br>Gradientstudierne indeholder målinger af: <br><br>1) bentisk lysintensitet, <br><br>2) bundforhold, <br><br>3) opportunister,<br><br>4) epifytter, <br><br>5) DIN- og DIP-gradienter, <br><br>6) iltkoncentrationer, <br><br>7) temperaturer, <br><br>8) sandormetætheder, <br><br>9) flerårige makroalger. <br><br>Ved at sammenholde de individuelle og kombinerede presfaktorer med vegetative skudtilvækster og -tab er der fundet tærskelværdier for de fleste parametre. <br><br>Hvert år afsættes der 2 uger til citizen science baseret ålegræsudplantningsaktiviteter, hvorved der foregår en løbende udbygning af ålegræsområder. Der er også anlagt mindre stenrev i yderfjorden samt udsat 20.000 skrubber. På udvalgte produktive lavbundslokaliteter indsamles der vandprøver som analyseres på SDU."},
+    "geometry":{"type":"Point",
+    "coordinates":[9.954271168666232,55.848164466078813]}}]}
